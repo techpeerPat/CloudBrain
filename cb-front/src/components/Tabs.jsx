7ba89e30d22bbf31056c93e907682a5d6afc8a9c
Keka321 <123451611+Keka321@users.noreply.github.com>
2024-04-09 13:02:19 +0200

    Tabs

--- cb-front/src/components/Tabs.jsx
+++ cb-front/src/components/Tabs.jsx
@@ -3,61 +3,53 @@
 import React from "react";
 import * as Tabs from "@radix-ui/react-tabs";
 
-const TabsDemo = ({ content }) => (
-  <Tabs.Root orientation="horizontal" defaultValue="tab1">
-    <Tabs.List
-      className="shrink-0 flex border-b border-mauve6"
-      aria-label="Manage your account"
-    >
-      {/* {content.map((item) => {
+const TabsDemo = ({ categories, content }) => (
+  console.log(content),
+  (
+    <Tabs.Root defaultValue="tab1">
+      <Tabs.List
+        className="shrink-0 flex border-b border-mauve6"
+        aria-label="Manage your account"
+      >
         <Tabs.Trigger
-          value={item.title}
+          value="all"
           className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
         >
-          <p>{item.category}</p>
-        </Tabs.Trigger>;
-      })} */}
-    </Tabs.List>
-    <Tabs.Content
-      className="grow p-5 bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
-      value="tab1"
-    >
-      <p className="mb-5 text-mauve11 text-[15px] leading-normal">
-        Make changes to your account here. Click save when you're done.
-      </p>
-      <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-        <label
-          className="text-[13px] leading-none mb-2.5 text-violet12 block"
-          htmlFor="name"
+          all
+        </Tabs.Trigger>
+        {categories.map((category) => (
+          <Tabs.Trigger
+            value={category.title}
+            className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+          >
+            {category.title}
+          </Tabs.Trigger>
+        ))}
+      </Tabs.List>
+
+      {content.map((item) => (
+        <Tabs.Content
+          value="all"
+          className="grow p-5 bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
         >
-          Name
-        </label>
-        <input
-          className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-          id="name"
-          defaultValue="Pedro Duarte"
-        />
-      </fieldset>
-      <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-        <label
-          className="text-[13px] leading-none mb-2.5 text-violet12 block"
-          htmlFor="username"
+          <div>
+            <p>{item.title}</p>
+          </div>
+        </Tabs.Content>
+      ))}
+
+      {content.map((item) => (
+        <Tabs.Content
+          value="Foundations"
+          className="grow p-5 bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
         >
-          Username
-        </label>
-        <input
-          className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-          id="username"
-          defaultValue="@peduarte"
-        />
-      </fieldset>
-      <div className="flex justify-end mt-5">
-        <button className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-green4 text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default">
-          Save changes
-        </button>
-      </div>
-    </Tabs.Content>
-  </Tabs.Root>
+          <div>
+            <p>{item.title}</p>
+          </div>
+        </Tabs.Content>
+      ))}
+    </Tabs.Root>
+  )
 );
 
 export default TabsDemo;
